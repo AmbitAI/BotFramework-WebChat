ce41c2363a8cc788faee4b66d9d3679ce9efaf36
Thomas Clarkson <tom@designsight.co.nz>
2017-12-07 15:46:53 +1300

    ready for unpkg

--- src/BotChat.ts
+++ src/BotChat.ts
@@ -5,6 +5,7 @@ export { queryParams } from './Attachment';
 export { SpeechOptions } from './SpeechOptions'
 export { Speech } from './SpeechModule'
 export { ChatWindow } from './ChatWindow';
+export { renderChatWindow } from './renderChatWindow'; 
 
 // below are shims for compatibility with old browsers (IE 10 being the main culprit)
 import 'core-js/modules/es6.string.starts-with';
